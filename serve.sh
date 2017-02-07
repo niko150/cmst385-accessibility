66e3186848b46d1b61884b728f1a0b8ee969fbf0
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-02-07 06:41:46 -0500

    trying dev setup again

--- serve.sh
+++ serve.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
 
 if [ ! -f './dist/server.js' ]; then
+export NODE_ENV=development;
+export NPM_CONFIG_PRODUCTION=false;
     npm clean;
+export NODE_ENV=production;
+export NPM_CONFIG_PRODUCTION=false;
     npm build
 fi
 
