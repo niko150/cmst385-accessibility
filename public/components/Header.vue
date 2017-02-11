8f263bfd5b7ed98f98940650a100de64f4ca76d6
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2017-02-11 12:58:07 -0500

    Added and tested tabindex

--- public/components/Header.vue
+++ public/components/Header.vue
@@ -4,14 +4,14 @@
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-          <router-link to="/" class="navbar-brand" v-bind:tabindex="home?-1:0">CMST 385 | Accessibility</router-link>
+          <router-link to="/" class="navbar-brand" v-bind:tabindex="home?-1:0">CMST 385 - Accessibility</router-link>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <router-link to="/classes" class="btn" active-class="btn-raised" v-bind:tabindex="tabIndex('/classes')" tag="li"><a>Classes</a></router-link>
-            <router-link to="/about" class="btn" active-class="btn-raised" v-bind:tabindex="tabIndex('/about')" tag="li"><a>About</a></router-link>
+            <router-link to="/classes" class="btn" active-class="btn-raised" tag="li"><a v-bind:tabindex="tabIndex('/classes')">Classes</a></router-link>
+            <router-link to="/about" class="btn" active-class="btn-raised" tag="li"><a v-bind:tabindex="tabIndex('/about')">About</a></router-link>
           </ul>
         </div>
       </div>
@@ -22,13 +22,13 @@
 <script>
 export default {
   computed: { 
-    home: () => this.$route.path === '/'
+    home() { return this.$route.path === '/' }
   },
   methods: {
     tabIndex(link) {
       if (!this.home)
         return ''
-      return link === '/about' ? 2: 3 
+      return link === '/about' ? 3: 2
     }
   }
 }
--- public/components/Home.vue
+++ public/components/Home.vue
@@ -6,6 +6,15 @@
     <h2>Supporting Evidence of Accessibility</h2>
     <hr />
     <br />
+    <p>The header here contains 3 links, navigating to the pages of the site. The tabindex attribute is set programatically to aid accessibility for users with needs for enhanced keyboard navigation. On the home screen (here), the home link will not recieve focus. On the other two pages it will. Other content loaded in the header above these links will not displace the classes and about links in the tab order. To see where the tab focus is currently, a toggle is provided.</p>
+    <div class="togglebutton">
+      <label>
+        Show Focus
+        <input type="checkbox" v-bind:checked="focus" v-on:change="toggleFocus()"><span class="toggle"></span>
+      </label>
+      <p><span style="font-style: italic">focus will render only with pressing tab after this is enabled.</span></p>
+    </div>
+    <br />
     <div>
     <router-link to='/about' id="about" class="btn-info">See what goes into accessibility at this site</router-link>
     </div>
@@ -13,6 +22,20 @@
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      focus: false
+    }
+  },
+  methods: {
+    toggleFocus() {
+      const rule = ':focus { border: solid thin white}'
+      this.focus = !this.focus
+      focus? document.styleSheets[0].insertRule(rule, 0): CSSStyleSheer.deleteRule(0)
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -38,5 +61,92 @@ h2 {
   margin-bottom: 0.25rem;
   line-height: 1.1;
   color: inherit;
+}
+
+.togglebutton {
+  vertical-align: middle;
+}
+.togglebutton,
+.togglebutton label,
+.togglebutton input,
+.togglebutton .toggle {
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+          user-select: none;
+}
+.togglebutton label {
+  cursor: pointer;
+  color: rgba(0,0,0, 0.26);
+}
+.form-group.is-focused .togglebutton label {
+  color: rgba(0,0,0, 0.26);
+}
+.form-group.is-focused .togglebutton label:hover,
+.form-group.is-focused .togglebutton label:focus {
+  color: rgba(0,0,0, .54);
+}
+fieldset[disabled] .form-group.is-focused .togglebutton label {
+  color: rgba(0,0,0, 0.26);
+}
+.togglebutton label input[type=checkbox] {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+.togglebutton label .toggle {
+  text-align: left;
+}
+.togglebutton label .toggle,
+.togglebutton label input[type=checkbox][disabled] + .toggle {
+  content: "";
+  display: inline-block;
+  width: 30px;
+  height: 15px;
+  background-color: rgba(80, 80, 80, 0.7);
+  border-radius: 15px;
+  margin-right: 15px;
+  -webkit-transition: background 0.3s ease;
+       -o-transition: background 0.3s ease;
+          transition: background 0.3s ease;
+  vertical-align: middle;
+}
+.togglebutton label .toggle:after {
+  content: "";
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-color: #F1F1F1;
+  border-radius: 20px;
+  position: relative;
+  -webkit-box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.4);
+          box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.4);
+  left: -5px;
+  top: -2px;
+  -webkit-transition: left 0.3s ease, background 0.3s ease, -webkit-box-shadow 0.1s ease;
+       -o-transition: left 0.3s ease, background 0.3s ease, box-shadow 0.1s ease;
+          transition: left 0.3s ease, background 0.3s ease, box-shadow 0.1s ease;
+}
+.togglebutton label input[type=checkbox][disabled] + .toggle:after,
+.togglebutton label input[type=checkbox][disabled]:checked + .toggle:after {
+  background-color: #BDBDBD;
+}
+.togglebutton label input[type=checkbox] + .toggle:active:after,
+.togglebutton label input[type=checkbox][disabled] + .toggle:active:after {
+  -webkit-box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.4), 0 0 0 15px rgba(0, 0, 0, 0.1);
+          box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.4), 0 0 0 15px rgba(0, 0, 0, 0.1);
+}
+.togglebutton label input[type=checkbox]:checked + .toggle:after {
+  left: 15px;
+}
+.togglebutton label input[type=checkbox]:checked + .toggle {
+  background-color: rgba(0, 150, 136, 0.5);
+}
+.togglebutton label input[type=checkbox]:checked + .toggle:after {
+  background-color: #009688;
+}
+.togglebutton label input[type=checkbox]:checked + .toggle:active:after {
+  -webkit-box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.4), 0 0 0 15px rgba(0, 150, 136, 0.1);
+          box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.4), 0 0 0 15px rgba(0, 150, 136, 0.1);
 }
 </style>
